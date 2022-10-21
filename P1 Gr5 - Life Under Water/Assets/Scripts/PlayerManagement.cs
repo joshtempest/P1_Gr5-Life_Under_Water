a3a276e885ef5c9ccbc7b52f164b79aaf3ee3fc5
HaXr <haxr@outlook.dk>
2022-10-21 15:50:48 +0200

    Updates to PlayerManagement script
    - Added score functionality
    - Changed how the size is calulated. 'playerSize' is now based on the 'score' devided by a new 'sizeIncrement' variable.
    - Now prints 'score' and 'playerSize' to the console.

--- P1 Gr5 - Life Under Water/Assets/Scripts/PlayerManagement.cs
+++ P1 Gr5 - Life Under Water/Assets/Scripts/PlayerManagement.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class PlayerManagement : MonoBehaviour
 {
     //Based on "How to make an object follow the mouse in Unity" by Karolio: https://www.youtube.com/watch?v=mF_BB_YsyDk
     Camera cam;
-    public Vector3 sizeUp;
+
+    int score = 1;  //This variable keeps track of the score
+    public float sizeIncrement; //Determins amount of size increase (Lower number = Bigger increase)
+    float playerSize; //Used to Determine the size of the player object.
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +25,19 @@ public class PlayerManagement : MonoBehaviour
         transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
     }
 
+    //This function deals with everything happening when colliding with an object
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Object")
+        if (other.gameObject.tag == "Object") //If collided object have the tag "Object"
         {
-            transform.localScale += sizeUp;
-            Destroy(other.gameObject);
+            score = score + 1; //Add +1 to the score
+
+            playerSize = 1 + (score / sizeIncrement); // Determines the size of the player object. Devides score by sizeUp to control growth of the player object
+            Vector3 sizeVector = new Vector3(playerSize, playerSize, 0); //Creates a new vecter called "sizeVector" which is based on the size variable.
+            transform.localScale = sizeVector; //Uses the sizeVector to grow the player object. (Sets scale of object to sizeVector's values)
+            Debug.Log("Score = " + score); //Prints score to console
+            Debug.Log("Player Size = " + playerSize); //Prints playerSize to console
+            Destroy(other.gameObject); //Destroy the collided object
         }
     }
 }
