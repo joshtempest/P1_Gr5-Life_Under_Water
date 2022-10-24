55776b0675fd416cf34488f023c08661085a465e
HaXr <haxr@outlook.dk>
2022-10-24 13:57:42 +0200

    Updates to PlayerManagement script
    Made several adjustments to the PlayerManagement script to allow for adding multiple types of objects down the line that have different point values.
    - Moved calculation of score to its own 'ComputeScore' function.
    - Moved player size adjustment to its own 'SizeController' function.
    - Added new object type with its own prefab and tag "Object2".

--- P1 Gr5 - Life Under Water/Assets/Scripts/PlayerManagement.cs
+++ P1 Gr5 - Life Under Water/Assets/Scripts/PlayerManagement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using UnityEngine;
 
 public class PlayerManagement : MonoBehaviour
@@ -11,7 +12,7 @@ public class PlayerManagement : MonoBehaviour
     int score = 1;  //This variable keeps track of the score
     public float sizeIncrement; //Determins amount of size increase (Lower number = Bigger increase)
     float playerSize; //Used to Determine the size of the player object.
-
+    int points;
 
     // Start is called before the first frame update
     void Start()
@@ -30,14 +31,40 @@ public class PlayerManagement : MonoBehaviour
     {
         if (other.gameObject.tag == "Object") //If collided object have the tag "Object"
         {
-            score = score + 1; //Add +1 to the score
+            points = 1;
 
-            playerSize = 1 + (score / sizeIncrement); // Determines the size of the player object. Devides score by sizeUp to control growth of the player object
-            Vector3 sizeVector = new Vector3(playerSize, playerSize, 0); //Creates a new vecter called "sizeVector" which is based on the size variable.
-            transform.localScale = sizeVector; //Uses the sizeVector to grow the player object. (Sets scale of object to sizeVector's values)
-            Debug.Log("Score = " + score); //Prints score to console
-            Debug.Log("Player Size = " + playerSize); //Prints playerSize to console
+            score = ComputeScore(score, points);
+
+            SizeController();
             Destroy(other.gameObject); //Destroy the collided object
         }
+
+        if (other.gameObject.tag == "Object2") //If collided object have the tag "Object"
+        {
+            points = 10;
+
+            score = ComputeScore(score, points);
+
+            SizeController();
+            Destroy(other.gameObject); //Destroy the collided object
+        }
+
     }
+
+    int ComputeScore(int a, int b) //Adds the points variable to the score
+    {
+        int computedScore = a + b;
+
+        return computedScore;
+    }
+
+    void SizeController()
+    {
+        playerSize = 1 + (score / sizeIncrement); // Determines the size of the player object. Devides score by sizeUp to control growth of the player object
+        Vector3 sizeVector = new Vector3(playerSize, playerSize, 0); //Creates a new vecter called "sizeVector" which is based on the size variable.
+        transform.localScale = sizeVector; //Uses the sizeVector to grow the player object. (Sets scale of object to sizeVector's values)
+        Debug.Log("Score = " + score); //Prints score to console
+        Debug.Log("Player Size = " + playerSize); //Prints playerSize to console
+    }
+
 }
