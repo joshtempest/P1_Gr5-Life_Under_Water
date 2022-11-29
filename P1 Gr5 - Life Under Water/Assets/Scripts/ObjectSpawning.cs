62b934b9f1c7f648d095750df916bef4c63d02e3
HaXr <haxr@outlook.dk>
2022-11-29 12:45:35 +0100

    Improved Spawning

--- P1 Gr5 - Life Under Water/Assets/Scripts/ObjectSpawning.cs
+++ P1 Gr5 - Life Under Water/Assets/Scripts/ObjectSpawning.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class ObjectSpawning : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
     public float spawnSpeed;
     public int spawnAmount;
+    public float minDistanceFromPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("Generate", 0, spawnSpeed);
+        //InvokeRepeating("SpawnObject", 0, spawnSpeed);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -21,15 +25,30 @@ public class ObjectSpawning : MonoBehaviour
 
     void Generate()
     {
-        if(GameObject.FindGameObjectsWithTag(ObjectToSpawn.tag).Length < spawnAmount) 
-        { 
-            int x = Random.Range(0, Camera.main.pixelWidth);
-            int y = Random.Range(0, Camera.main.pixelHeight);
+        //Vector3 spawnPosition = new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0f);
 
-            Vector3 Target = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0));
-            Target.z = 0;
+        if (GameObject.FindGameObjectsWithTag(ObjectToSpawn.tag).Length < spawnAmount)
+        {
+            int x = Random.Range(-1 * Camera.main.pixelWidth, Camera.main.pixelWidth * 2);
+            int y = Random.Range(-1 * Camera.main.pixelHeight, Camera.main.pixelHeight * 2);
 
-            Instantiate(ObjectToSpawn, Target, Quaternion.identity); //Clones the GameObject prefab.
-        }
-    }
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0));
+            spawnPosition.z = 0;
+
+            Vector3 playerPosition = GameObject.Find("Player").transform.position; //Gets the position of the player object.
+
+            Vector3 distanceFromPlayer = playerPosition - spawnPosition; //Calculates the differentiating vector between playerPosition and spawnPosition.
+
+            float fdistance = Mathf.Sqrt(Mathf.Pow(distanceFromPlayer.x, 2) + Mathf.Pow(distanceFromPlayer.y, 2)); //Finds pythagoras to find the distance to the player as a float
+
+            print(fdistance);
+
+            if (fdistance > minDistanceFromPlayer) //Spawns object if the distance to the player is great enough.
+            {
+                Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity); //Clones the GameObject prefab.
+            }
+
+           
+       }
+   }
 }
---
+++ P1 Gr5 - Life Under Water/Assets/Scripts/PreyBehavior.cs
@@ -0,0 +1,59 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class PreyBehavior : MonoBehaviour
+{
+    public Transform player;
+
+    public int newScore;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 1f;
+    public float lookRadius = 10f;
+    public float pickUpRadius = 15f;
+    NavMeshAgent agent;
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    //
+    private void FixedUpdate() //Using FixedUpdate here so that the prey stops moving when the game is paused.
+    {
+        Vector3 distance = player.position - transform.position; //Finds the distance between prey and player
+        float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg; //Gets the angle between the prey and the player in radians which is then converted
+        rb.rotation = angle; //rotates to face the player
+        float fdistance = Mathf.Sqrt(Mathf.Pow(distance.x, 2) + Mathf.Pow(distance.y, 2)); //Finds pythagoras to find the distance to the player as a float
+        distance.Normalize(); //Normalizes the distance vector, since we only need the direction
+        movement = -1 * distance; //Makes the prey move away from the player.
+
+        ///<Summary>
+        ///Finds the player so that the player score can be used to compare the prey score with the player score
+        /// </Summary>
+        
+        GameObject Player = GameObject.Find("Player");
+        PlayerManager playerManagement = Player.GetComponent<PlayerManager>();
+
+        if (fdistance <= lookRadius) //Moves towards the player if enemy score is higher than player and within it's look radius
+        {
+            moveCharacter(movement);
+        }
+    }
+    void moveCharacter(Vector2 distance)
+    {
+        rb.MovePosition((Vector2)transform.position + (distance * moveSpeed * Time.deltaTime));
+    }
+    private void OnDrawGizmosSelected() //Visualises the look radius to help with making and testing the game
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
