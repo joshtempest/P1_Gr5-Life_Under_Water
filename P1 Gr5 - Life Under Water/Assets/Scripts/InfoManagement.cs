83cd8a395a85858acc723c09550a3f8c39bd5bf9
Adrenalina333 <115779476+Adrenalina333@users.noreply.github.com>
2022-12-05 20:59:36 +0100

    InfoManager
    Virker halt...

--- P1 Gr5 - Life Under Water/Assets/Scripts/InfoManagement.cs
+++ P1 Gr5 - Life Under Water/Assets/Scripts/InfoManagement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using TMPro;
 
@@ -10,8 +11,11 @@ public class InfoManagement : MonoBehaviour
     public GameObject menuController;
     MenuManager mm;
     static int infoIndex = 0;
+    public int i = 0;
     public TextMeshProUGUI currentInfoText;
     public TextMeshProUGUI[] infoTexts;
+
+    [SerializeField] int [] currentWeightInfo = {5, 10, 20, 30, 40};
 
     private void Start()
     {
@@ -21,11 +25,44 @@ public class InfoManagement : MonoBehaviour
 
     public void Update()
     {
-        if (pm.score >= pm.sizeLimits[infoIndex] && infoIndex < 4)
+        
+        //for (int i = 0; i < currentWeightInfo.Length; i++); 
+
+        if (pm.score >= currentWeightInfo[0] && infoIndex < 4)
         {
             ShowInfo();
             infoIndex++;
+            currentWeightInfo[0] = currentWeightInfo[1];
         }
+        else if (pm.score >= currentWeightInfo[0] && pm.score < currentWeightInfo[1] && infoIndex < 4)
+        {
+            ShowInfo();
+            infoIndex++;
+            currentWeightInfo[0] = currentWeightInfo[1];
+
+        }
+        else if (pm.score >= currentWeightInfo[1] && pm.score < currentWeightInfo[2] && infoIndex < 4)
+        {
+            ShowInfo();
+            infoIndex++;
+            currentWeightInfo[1] = currentWeightInfo[2];
+
+        }
+        else if (pm.score >= currentWeightInfo[2] && pm.score < currentWeightInfo[3] && infoIndex < 4)
+        {
+            ShowInfo();
+            infoIndex++;
+            currentWeightInfo[2] = currentWeightInfo[3];
+
+        }
+        else if (pm.score >= currentWeightInfo[3] && pm.score < currentWeightInfo[4] && infoIndex < 4)
+        {
+            ShowInfo();
+            infoIndex++;
+            currentWeightInfo[3] = currentWeightInfo[4];
+
+        }
+      
     }
 
     public void ShowInfo()
@@ -33,4 +70,28 @@ public class InfoManagement : MonoBehaviour
         mm.InfoMenu();
         currentInfoText.text = infoTexts[infoIndex].text;
     }
+
+
+    //Indsætte et array af int, som mankan kontrolerer defra. Lav den enten public eller serialized field, så kan man lndre det fra inspectoren. 
+
+     /*public void Update()
+    {
+         if (pm.score >= pm.sizeLimits[infoIndex] && infoIndex < 4)
+        {
+            ShowInfo();
+            infoIndex++;
+
+            pm.score[currentWeightInfo]
+        }
+    }
+
+      else if (pm.score >= currentWeightInfo [0] && pm.score < currentWeightInfo[1] && infoIndex < 4)
+        {
+            ShowInfo();
+            infoIndex++;
+            currentWeightInfo[i] = currentWeightInfo[1];
+        }
+
+    */
+
 }
