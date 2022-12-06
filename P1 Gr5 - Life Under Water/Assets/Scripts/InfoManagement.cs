27f323459e93ae33ff9cb2e377b3a1ff7e254161
Adrenalina333 <115779476+Adrenalina333@users.noreply.github.com>
2022-12-06 15:00:52 +0100

    Update InfoManagement.cs

--- P1 Gr5 - Life Under Water/Assets/Scripts/InfoManagement.cs
+++ P1 Gr5 - Life Under Water/Assets/Scripts/InfoManagement.cs
@@ -40,27 +40,4 @@ public class InfoManagement : MonoBehaviour
         currentInfoText.text = infoTexts[infoIndex].text;
     }
 
-
-    //Indsætte et array af int, som mankan kontrolerer defra. Lav den enten public eller serialized field, så kan man lndre det fra inspectoren. 
-
-     /*public void Update()
-    {
-         if (pm.score >= pm.sizeLimits[infoIndex] && infoIndex < 4)
-        {
-            ShowInfo();
-            infoIndex++;
-
-            pm.score[currentWeightInfo]
-        }
-    }
-
-      else if (pm.score >= currentWeightInfo [0] && pm.score < currentWeightInfo[1] && infoIndex < 4)
-        {
-            ShowInfo();
-            infoIndex++;
-            currentWeightInfo[i] = currentWeightInfo[1];
-        }
-
-    */
-
 }
