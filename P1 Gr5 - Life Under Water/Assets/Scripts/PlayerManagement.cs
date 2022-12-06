ad398768e453106849475ed68c73902f74030452
Carolinebchr <115777972+Carolinebchr@users.noreply.github.com>
2022-12-06 13:49:37 +0100

    cola added

---
+++ P1 Gr5 - Life Under Water/Assets/Scripts/PlayerManagement.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+
+public class PlayerManagement : MonoBehaviour
+{
+    //Loosly based on "How to make an object follow the mouse in Unity" by Karolio: https://www.youtube.com/watch?v=mF_BB_YsyDk
+    //(Maybe unnecessary) Includes elements based on "How to change a Sprite from a script in Unity (with examples)" by gamedevbeginner: https://gamedevbeginner.com/how-to-change-a-sprite-from-a-script-in-unity-with-examples/
+    //Includes elements based on "HOW TO ACCESS DATA FROM ANOTHER SCRIPT 🎮 | Get Data From Other Scripts In Unity | Unity Tutorial" by Dani Krossing: https://www.youtube.com/watch?v=Y7pp2gzCzUI
+
+    public int score = 1;  //This variable keeps track of the score
+    public float sizeIncrement; //Determins amount of size increase (Lower number = Bigger increase)
+    float playerSize; //Used to Determine the size of the player object.
+    public float speed; //Determines the speed of the player object.
+    
+    public VariableJoystick variableJoystick; //Calls the VariableJoystick script which is attached to the joystick itself.
+    public Rigidbody2D RIGIDBODY; //Calls the rigidbody of the player object.
+
+    SpriteRenderer sr; //The sprite renderer of the player.
+    public Sprite[] playerSprites; //The collection of sprites for the player.
+    public int[] sizeLimits; //The collection of sizes that are required to make the sprite change.
+    SceneControls sceneControls; //In order to access the methods in "SceneControls".
+    [SerializeField] GameObject sceneController; //The game object that has the script "SceneControls".
+    UpgradeManagement upgradeManagement; //In order to access the methods in "UpgradeManagement".
+    EnemyBehavior enemyBehavior;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>(); //The sprite renderer of the player.
+        sr.sprite = playerSprites[0]; //Set the player sprite as the first one.
+        sceneControls = sceneController.GetComponent<SceneControls>(); //Gets the script "SceneControls" from the scene controller.
+        sceneControls.SetScoreText(score); //Sets up the score from the beginning
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+
+    //This function deals with everything happening when colliding with an object
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Object") //If collided object have the tag "Object"
+        {
+            int points = 1; //Local variable that determines how how the score should increase
+
+            score = ComputeScore(score, points); //Calls the 'ComputeScore' function to calculate the score
+            sceneControls.SetScoreText(score); //Calls the 'SetScoreText' function from "SceneControls" to set the score text.
+
+            SizeController(); //Runs "SizeController" to control the size.
+            Destroy(other.gameObject); //Destroy the collided object
+        }
+
+        if (other.gameObject.tag == "Object2") //If collided object have the tag "Object"
+        {
+            int points = 10; //Local variable that determines how how the score should increase
+
+            score = ComputeScore(score, points); //Calls the 'ComputeScore' function to calculate the score
+            sceneControls.SetScoreText(score); //Calls the 'SetScoreText' function from "SceneControls" to set the score text.
+
+            SizeController(); //Runs "SizeController" to control the size.
+            Destroy(other.gameObject); //Destroy the collided object
+        }
+
+        if (other.gameObject.tag == "Object3") //If collided object have the tag "Object"
+        {
+            int points = -1; //Local variable that determines how how the score should increase
+
+            score = ComputeScore(score, points); //Calls the 'ComputeScore' function to calculate the score
+            sceneControls.SetScoreText(score); //Calls the 'SetScoreText' function from "SceneControls" to set the score text.
+
+            SizeController(); //Runs "SizeController" to control the size.
+            Destroy(other.gameObject); //Destroy the collided object
+        }
+        if (other.gameObject.tag == "Enemy") //If collided object have the tag "Object"
+        {
+            int newEnemyScore = EnemyBehavior.enemyScore;
+            if (newEnemyScore >= score) 
+            {
+                int points = -score; //Local variable that determines how how the score should increase
+                score = ComputeScore(score, points); //Calls the 'ComputeScore' function to calculate the score
+                sceneControls.SetScoreText(score);
+                SizeController();
+                Destroy(other.gameObject); //Destroy the collided object
+            }
+            else
+            {
+                int point = newEnemyScore;
+                score = ComputeScore(score, point);
+                sceneControls.SetScoreText(score);
+                SizeController();
+                Destroy(other.gameObject);
+            } 
+
+        }
+    }
+
+    //This function is responsible for adding the points variable to the score
+    int ComputeScore(int a, int b)
+    {
+        int computedScore = a + b;
+
+        return computedScore;
+    }
+
+    //This function is responsible for adjusting the size of the player object
+    void SizeController() 
+    {
+        playerSize = 1 + (score / sizeIncrement); // Determines the size of the player object. Devides score by sizeUp to control growth of the player object
+        Vector3 sizeVector = new Vector3(playerSize, playerSize, 0); //Creates a new vecter called "sizeVector" which is based on the size variable.
+        transform.localScale = sizeVector; //Uses the sizeVector to grow the player object. (Sets scale of object to sizeVector's values)
+        Debug.Log("Score = " + score); //Prints score to console
+        Debug.Log("Player Size = " + playerSize); //Prints playerSize to console
+        SpriteController(); //Runs SpriteController function which is responsible for changing out the player sprites
+    }
+
+    //This function takes the joystick direction and translates it into motion of the player object.
+    public void FixedUpdate()
+    {
+        Vector2 direction = Vector2.up * variableJoystick.Direction + Vector2.right * variableJoystick.Direction;
+        RIGIDBODY.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode2D.Force);
+    }
+
+    //This is the function that buys increased speed when pushing the button in the upgrade menu.
+    public void BuyMoreSpeed()
+    {
+        float[] info = new float[2]; //An array to get both the new score and the new speed.
+        info = upgradeManagement.IncreaseSpeed(score,speed); //Calls "IncreaseSpeed" and contains the info in the array.
+        score = (int) info[0]; //Changes the score to the new value.
+        sceneControls.SetScoreText(score); //Sets the score text.
+        SizeController(); //Making sure the size of the player matches the new score.
+        speed = info[1]; //Changes the speed to the new value.
+    }
+
+    //This is the function that buys increased growth when pushing the button in the upgrade menu.
+    public void BuyMoreGrowth()
+    {
+        float[] info = new float[2]; //An array to get both the new score and the new size increment.
+        info = upgradeManagement.IncreaseGrowth(score,sizeIncrement); //Calls "IncreaseGrowth" and contains the info in the array.
+        score = (int)info[0]; //Changes the score to the new value.
+        sceneControls.SetScoreText(score); //Sets the score text.
+        SizeController(); //Making sure the size of the player matches the new score.
+        sizeIncrement = info[1]; //Changes the size increment to the new value.
+    }
+
+    private void SpriteController()
+    {
+        if (score < sizeLimits[0]) //Changes the sprite if score is under the first limit
+        {
+            sr.sprite = playerSprites[0];
+        }
+        else if (score >= sizeLimits[0] && score < sizeLimits[1]) //Changes the sprite if the score is between the first and second limit.
+        {
+            sr.sprite = playerSprites[1];
+        }
+        else if (score >= sizeLimits[1] && score < sizeLimits[2]) //Changes the sprite if the score is between the second and third limit.
+        {
+            sr.sprite = playerSprites[2];
+        }
+        else if (score >= sizeLimits[2] && score < sizeLimits[3]) ///Changes the sprite if the score is between the third and fourth limit.
+        {
+            sr.sprite = playerSprites[3];
+        }
+        else if (score >= sizeLimits[3]) //Changes the sprite if the score passes the fourth limit.
+        {
+            sr.sprite = playerSprites[4];
+        }
+        if (score == 0) //If the score reaches 0, then the game is over and the method is called in order to end it.
+        {
+            sceneControls.GameOver();
+        }
+    }
+}
---
+++ P1 Gr5 - Life Under Water/Assets/Scripts/SceneControls.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using System;
+using Unity.VisualScripting.Antlr3.Runtime;
+
+public class SceneControls : MonoBehaviour
+{
+    //Includes elements based on "Roll-a-Ball - Displaying Score and Text" by Unity: https://learn.unity.com/tutorial/displaying-score-and-text?uv=2019.4&projectId=5f158f1bedbc2a0020e51f0d
+    //(???) Includes elements based on "PauseScript" from "Game Project - Find The Portal" by Victor Hejø
+
+    public TextMeshProUGUI scoreText; //Text for the score.
+    public GameObject upgradeScreen; //The menu for upgrades.
+    public GameObject gameOverScreen; //The screen that shows when the game is over.
+    public GameObject upgradeMenuButton; //The button that opens and closes the upgrade menu.
+    bool isUpgrading; //A boolean that checks whether or not the upgrade menu is open
+    public GameObject pauseMenuButton; //Button for the pause button
+    public GameObject pauseMenu; //The pause menu
+
+    public static bool isPaused; //Responsible for pausing/unpausing the game.
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //Time.timeScale = 1; //Makes sure time passes normally and is not stuck when the level is potentially restarted.
+        gameOverScreen.SetActive(false); //Sets the game over screen to not be active, so the game can be played.
+        upgradeScreen.SetActive(false); //Deactivated the upgrade screen on start.
+        pauseMenu.SetActive(false);
+        //isUpgrading = false; //Makes sure the upgrade menu is off.
+        upgradeMenuButton.SetActive(true); //Set the upgrade button to be active
+        pauseMenuButton.SetActive(true);
+    }
+
+    //Loads to a specific scene (Specified in the inspector).
+    public void LoadScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    //Reloads the current level.
+    public void RestartGame()
+    {
+        LoadScene(SceneManager.GetActiveScene().buildIndex); //Loads the currently active scene (Meant for the game scene)
+        ResumeGame(); //Resumes the game to make sure it isn't paused.
+    }
+
+    //Sets up the score text to show the score
+    public void SetScoreText(int score)
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    //Pauses the game through time.timescale and makes the "Game Over Panel" appear.
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true); //Turns the game over sceen active
+        upgradeMenuButton.SetActive(false); //Shuts off the upgrade menu button.
+        pauseMenuButton.SetActive(false); //Shuts off the pause menu button.
+        PauseGame(); //Calls PauseGame() function to pause the game.
+    }
+
+
+    //Switches the upgrade menu on and off.
+    public void Upgrades()
+    {
+        if (isUpgrading) //If you are upgrading, then it will now be turned off
+        {
+            isUpgrading = false;
+            upgradeScreen.SetActive(false);
+            pauseMenuButton.SetActive(true);
+            ResumeGame(); //Calls function to resumes the game
+
+        }
+        else if (!isUpgrading) //If you are not upgrading, then it will now be turned on.
+        {
+            isUpgrading = true;
+            upgradeScreen.SetActive(true);
+            pauseMenuButton.SetActive(false);
+            PauseGame(); //Calls function to pauses the game
+        }
+    }
+
+    public void PauseMenu()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+            pauseMenu.SetActive(false);
+            pauseMenuButton.SetActive(true);
+            upgradeMenuButton.SetActive(true);
+            scoreText.gameObject.SetActive(true);
+            ResumeGame();
+            
+        }
+        else if (!isPaused)
+        {
+            isPaused = true;
+            pauseMenu.SetActive(true);
+            pauseMenuButton.SetActive(false);
+            upgradeMenuButton.SetActive(false);
+            scoreText.gameObject.SetActive(false);
+            PauseGame();
+
+        }
+    }
+
+    //Pauses the game when called
+    public void PauseGame()
+    {
+        Time.timeScale = 0f; //Stops the game
+        isPaused = true;
+        Debug.Log("Paused"); //Prints to console that the game is paused
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f; //Resumes the game
+        isPaused = false;
+        Debug.Log("Resumed"); //Prints to console that the game has resumed
+    }
+}
