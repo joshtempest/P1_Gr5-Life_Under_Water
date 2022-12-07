4fb185f5159c2bf0d88cbcc2d8257915c6b9026d
HaXr <haxr@outlook.dk>
2022-12-07 22:40:08 +0100

    The score number will now always be below "Vægt:"

--- P1 Gr5 - Life Under Water/Assets/Scripts/MenuManager.cs
+++ P1 Gr5 - Life Under Water/Assets/Scripts/MenuManager.cs
@@ -59,7 +59,7 @@ public class MenuManager : MonoBehaviour
     //Sets up the score text to show the score
     public void SetScoreText(int score)
     {
-        scoreText.text = "Vægt: " + score.ToString() + " kg";
+        scoreText.text = "Vægt:\n" + score.ToString() + " kg";
     }
 
     //Pauses the game through time.timescale and makes the "Game Over Panel" appear.
